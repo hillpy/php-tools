74ec22eb4c806c1fcea24c411bd1c83448dc6e2a
shinn lancelot <945226793@qq.com>
2018-11-20 09:17:16 +0800

    1. 修改.gitignore 2. 新增String类

---
+++ src/String.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace hillpy\phptools;
+
+class String extends Base
+{
+    /**
+     * 获取字符串
+     * @param int $length
+     */
+    public static function getNonce($type = 1, $length = 16)
+    {
+        $nonce = '';
+        switch ($type) {
+            case 1:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+                break;
+            case 2:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+                break;
+            case 3:
+                $string = '0123456789';
+                break;
+            default:
+                $string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        }
+        for ($i = 0; $i < $length; $i++) {
+            $nonce .= substr($string, mt_rand(0, strlen($string) - 1), 1);
+        }
+        return $nonce;
+    }
+}
